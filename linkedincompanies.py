c539ef3f445a247dd777e37a5cf137df53cfbbe4
Shreyash Paratkar <shreyasparatkar@gmail.com>
2020-09-10 16:20:31 -0500

    Adding code

--- linkedincompanies.py
+++ linkedincompanies.py
@@ -1,49 +1,50 @@
 from selenium import webdriver
 import csv
 import sys
+from url_parser import parser_requester
+from locations import location_parser
 
-def company_list(n, uname , pas):
 
-   driver = webdriver.Chrome('/Users/sadhanakesavan/Downloads/chromedriver') 
-   driver.get('https://www.linkedin.com')  
-   username = driver.find_element_by_name("session_key")
-   username.send_keys(uname)  
-   password = driver.find_element_by_name("session_password")
-   print(uname)
-   password.send_keys(pas) 
-   submit = driver.find_element_by_class_name("sign-in-form__submit-button")  
-   submit.click() 
+def company_list(n, uname, pas):
+    driver = webdriver.Chrome('/Users/sadhanakesavan/Downloads/chromedriver')
+    driver.get('https://www.linkedin.com')
+    username = driver.find_element_by_name("session_key")
+    username.send_keys(uname)
+    password = driver.find_element_by_name("session_password")
+    password.send_keys(pas)
+    submit = driver.find_element_by_class_name("sign-in-form__submit-button")
+    submit.click()
 
-   driver.get('https://www.linkedin.com/search/results/companies/?origin=SWITCH_SEARCH_VERTICAL')
+    driver.get('https://www.linkedin.com/search/results/companies/?origin=SWITCH_SEARCH_VERTICAL')
 
-   username = driver.find_element_by_class_name("reusable-search__entity-results-list") 
-   options = username.find_elements_by_tag_name("li") 
-   print(len(options))
-   csvfile = open('samplecomps2.csv', 'w', newline='')
-   pg = 0
-   while pg < n :
-    for option in options:
-     if len(option.text.split('\n')) > 2 : 
-      print(option.text)
-      ar = option.text.split('\n')
-      print(len(ar))
-      print(option.text)
-      print(len(ar))  
-      print("Company Name:")
-      y = driver.find_element_by_partial_link_text(ar[0])
-      x = y.get_attribute("href")
-      ar[2] = x
-      spamwriter = csv.writer(csvfile,quotechar='|', quoting=csv.QUOTE_MINIMAL)
-      new_arr=[]
-      new_arr.append(ar[0])
-      new_arr.append(ar[1])
-      new_arr.append(ar[2])
-      spamwriter.writerow(new_arr) 
-      print(x)
-    pg = pg + 1
-    driver.get('https://www.linkedin.com/search/results/companies/?origin=SWITCH_SEARCH_VERTICAL&page={}'.format(pg))
     username = driver.find_element_by_class_name("reusable-search__entity-results-list")
     options = username.find_elements_by_tag_name("li")
+    csvfile = open('samplecomps2.csv', 'w', newline='')
+    pg = 0
+    while pg < n:
+        for option in options:
+            try:
+                if len(option.text.split('\n')) > 2:
+                    ar = option.text.split('\n')
+                    y = driver.find_element_by_partial_link_text(ar[0])
+                    x = y.get_attribute("href")
+                    ar[2] = x
+                    spamwriter = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    new_arr = []
+                    new_arr.append(ar[0])
+                    new_arr.append(ar[1])
+                    new_arr.append(parser_requester(ar[0].strip()))
+                    new_arr.append(location_parser(ar[0].strip()))
+                    spamwriter.writerow(new_arr)
+                    print(ar[0])
+            except:
+                print("Couldn't process ", option)
+        pg = pg + 1
+        driver.get(
+            'https://www.linkedin.com/search/results/companies/?origin=SWITCH_SEARCH_VERTICAL&page={}'.format(pg))
+        username = driver.find_element_by_class_name("reusable-search__entity-results-list")
+        options = username.find_elements_by_tag_name("li")
+
 
 list_of_args = sys.argv
-company_list(int(list_of_args[1]),list_of_args[2],list_of_args[3])
+company_list(int(list_of_args[1]), list_of_args[2], list_of_args[3])
---
+++ locations.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib
+from bs4.element import Comment
+from mechanize import Browser
+import json
+
+
+mech = Browser()
+url = "http://www.biggestuscities.com/top-1000"
+page = mech.open(url)
+html = page.read()
+soup = BeautifulSoup(html, features="lxml")
+all_locations = []
+for row in soup.find("table").findAll('a', {'class': 'link'}):
+    if '/city/' in row['href']:
+        all_locations.append(row.text.strip())
+
+locations = ' office locations '
+tag_locations = ' locations '
+URL = 'https://www.google.com/search?q='
+
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def location_parser(company_name):
+    url = ""
+    final_list = []
+    try:
+        url = BeautifulSoup(requests.get(URL + company_name + locations).content, "html.parser").find('div', {'class': 'kCrYT'}).find_all('a')[0]['href']
+        url = url[7: url.index('&')]
+        page = BeautifulSoup(requests.get(url).content, "html.parser").find_all(text=True)
+        visible_texts = filter(tag_visible, page)
+        visible_texts = u" ".join(t.strip() for t in visible_texts)
+        for t in all_locations:
+            if t in visible_texts:
+                final_list.append(t)
+
+    except:
+        try:
+            url = BeautifulSoup(requests.get(URL + company_name + tag_locations).content, "html.parser").find('div', {'class': 'kCrYT'}).find_all('a')[0]['href']
+            url = url[7: url.index('&')]
+            page = BeautifulSoup(requests.get(url).content, "html.parser").find_all(text=True)
+            visible_texts = filter(tag_visible, page)
+            visible_texts = u" ".join(t.strip() for t in visible_texts)
+            for t in all_locations:
+                if t in visible_texts:
+                    final_list.append(t)
+
+        except:
+            print(company_name, " NOT FOUND!")
+
+    return final_list
+
+
+all_data = {}
+with open('companies') as f:
+    companies = list(map(lambda x: x.strip(), f.readlines()))
+    for company in companies[:10]:
+        career_url, career_locations = location_parser(company)
+        all_data[company] = {"Career_URL": career_url, "Career_Locations": career_locations}
+
+with open('data.json', 'w') as f:
+    json.dump(all_data, f)
