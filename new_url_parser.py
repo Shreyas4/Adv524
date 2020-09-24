0d53f53bacad38f93d672b3599f7a67fdd52b5d8
Shreyash Paratkar <shreyasparatkar@gmail.com>
2020-09-24 18:13:35 -0500

    Refactor 1

--- new_url_parser.py
+++ new_url_parser.py
@@ -2,11 +2,11 @@ import pandas as pd
 from url_parser import parser_requester
 import csv
 
-df = pd.read_csv('us_companies.csv')
+companies = list(pd.read_csv('stock_data/us_companies.csv').ix[:, 1])
+companies = [x.strip() for x in list]
 csvfile = open('result_url_file.csv', 'w', newline='')
-for i, r in df.iterrows():
+for c in companies:
     spamwriter = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    company_name = r[1].strip()
-    nr = [company_name, parser_requester(company_name)]
+    nr = [c, parser_requester(c)]
     print(nr)
     spamwriter.writerow(nr)
