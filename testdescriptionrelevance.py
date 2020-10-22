0f2396741ed79f37bc02234d8be05bbb3f8b22c1
sadhana-kesavan <sadhana.kesavan@stonybrook.edu>
2020-10-22 02:13:35 -0400

    adding new 80% string match file for stock and relevance

--- testdescriptionrelevance.py
+++ testdescriptionrelevance.py
@@ -4,8 +4,8 @@ from relevancemodel import topic_model_train,predict
 ##this script adds the prediction values of relevance for each company
 ## > 0.7 seems to be a good relevance for software 
 model = topic_model_train(5,5)
-csvfile = open('samplecomps3.csv', 'r', newline='')
-filetowrite = open('samplecomps4.csv','w',newline='')
+csvfile = open('samplespecialties.csv', 'r', newline='')
+filetowrite = open('samplecompsspecial.csv','w',newline='')
 csvwriter = csv.writer(filetowrite, quotechar='|', quoting=csv.QUOTE_MINIMAL)
 lines = csvfile.readlines()
 firstpass = False
@@ -15,7 +15,10 @@ for l in lines:
    print(l)
    if 'Overview' in l:
         if firstpass:
-          rel = predict(word,model)
+          if word == '':
+           rel = 0.0
+          else:
+           rel = predict(word,model)
           new_arr.append(rel)
           csvwriter.writerow(new_arr)
         firstpass=True
